cbcd5481ce6925ecd8dfdc3b20610a6708f73152
joelitok <joelnkouatchet@gmail.com>
2022-02-22 17:12:41 +0100

    add information

--- src/main/java/com/secur/securite/sec/JWTAuthorizationFilter.java
+++ src/main/java/com/secur/securite/sec/JWTAuthorizationFilter.java
@@ -1,6 +1,6 @@
 package com.secur.securite.sec;
 
-import java.io.IOException;
+/*import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -25,21 +25,8 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-        /*
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
-       */ 
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-  response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
-  response.setHeader("Access-Control-Max-Age", "3600");
-  response.setHeader("Access-Control-Allow-Credentials", "true");
-  response.setHeader("Access-Control-Expose-Headers", "Authorization");
-  response.addHeader("Access-Control-Expose-Headers", "responseType");
-  response.addHeader("Access-Control-Expose-Headers", "observe");
-        
-        
+
+
         if (request.getMethod().equals("OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
@@ -49,10 +36,6 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
                 filterChain.doFilter(request, response);
                 return;
           }
-
-
-
-
 
 
 		// first metho
@@ -88,4 +71,85 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
 	}
 }
 
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, authorization");
+        if(request.getMethod().equals("OPTIONS")){
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        else if(request.getRequestURI().equals("/login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        else {
+            String jwtToken = request.getHeader(SecurityConstants.HEADER_STRING);
+            System.out.println("Token="+jwtToken);
+            if (jwtToken == null || !jwtToken.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SecurityConstants.SECRET)).build();
+            String jwt = jwtToken.substring(SecurityConstants.TOKEN_PREFIX.length());
+            DecodedJWT decodedJWT = verifier.verify(jwt);
+            System.out.println("JWT="+jwt);
+            String username = decodedJWT.getSubject();
+            List<String> roles = decodedJWT.getClaims().get("roles").asList(String.class);
+            System.out.println("username="+username);
+            System.out.println("roles="+roles);
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            roles.forEach(rn -> {
+                authorities.add(new SimpleGrantedAuthority(rn));
+            });
+            UsernamePasswordAuthenticationToken user =
+                    new UsernamePasswordAuthenticationToken(username, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(user);
+            filterChain.doFilter(request, response);
+        }
+
+    }
 }
